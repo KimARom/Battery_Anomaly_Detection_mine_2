d1f7ed1180be769820814991b0f6bd4cbccd7549
Kim A <kimarom0103@outlook.kr>
2025-03-15 14:20:51 +0900

    .더추가

--- Dashboard/libraries/load_data.py
+++ Dashboard/libraries/load_data.py
@@ -6,7 +6,7 @@ def load_data(file_name):
     # file_list = os.listdir("./Data")
     # file = [file for file in file_list if str(data_num) in file]
     # df_dir = "./Data/" + file[0]
-    return pd.read_csv('/Dashboard/test.csv')
+    return pd.read_csv('./Dashboard/test.csv')
 
 def save_uploaded_file(directory, file):
     if not os.path.exists(directory):
